cccd257e1d45b626a67168239e58690155a52da6
danaska <danaska.alonso@gmail.com>
2020-05-03 19:27:11 +0200

    añadida primera prueba y su documentacion

--- src/ASumar.java
+++ src/ASumar.java
@@ -1,4 +1,23 @@
 
 public class ASumar {
+	
+	public ASumar() {
+		
+	}
+
+   /**
+    * mostrar es un método para mostrar el numero pasado como parametro.
+    * Si solo se le pasa un numero este devuelve " numero = numero"
+    * 
+    * @param String cadena de numeros	
+    * @return String con el la suma de todos los numeros y el total
+	*/
+	public String mostrar(String cad) {
+		String numero = cad;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+        return null;
+	}
 
 }
